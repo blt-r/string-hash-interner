effa6539a78e99812c15e585c0059cc588322cdd
blt__ <www.edav@gmail.com>
2025-02-24 02:29:31 +0400

    Fix benchmark

--- benches/bench.rs
+++ benches/bench.rs
@@ -16,14 +16,14 @@ criterion_group!(
 criterion_group!(bench_get, bench_get_already_filled);
 criterion_group!(bench_iter, bench_iter_already_filled);
 criterion_group!(
-    bench_intern,
-    bench_intern_fill,
-    bench_intern_fill_with_capacity,
-    bench_intern_already_filled,
+    bench_get_or_intern,
+    bench_get_or_intern_fill,
+    bench_get_or_intern_fill_with_capacity,
+    bench_get_or_intern_already_filled,
 );
-criterion_main!(bench_intern, bench_resolve, bench_get, bench_iter);
+criterion_main!(bench_get_or_intern, bench_resolve, bench_get, bench_iter);
 
-fn bench_intern_fill_with_capacity(c: &mut Criterion) {
+fn bench_get_or_intern_fill_with_capacity(c: &mut Criterion) {
     let mut g = c.benchmark_group("intern/fill-empty/with_capacity");
     g.throughput(Throughput::Elements(BENCH_LEN_STRINGS as u64));
     fn bench_for_backend<BB: BackendBenchmark>(g: &mut BenchmarkGroup<WallTime>) {
@@ -47,7 +47,7 @@ fn bench_intern_fill_with_capacity(c: &mut Criterion) {
     bench_for_backend::<BenchString>(&mut g);
 }
 
-fn bench_intern_fill(c: &mut Criterion) {
+fn bench_get_or_intern_fill(c: &mut Criterion) {
     let mut g = c.benchmark_group("intern/fill-empty/new");
     g.throughput(Throughput::Elements(BENCH_LEN_STRINGS as u64));
     fn bench_for_backend<BB: BackendBenchmark>(g: &mut BenchmarkGroup<WallTime>) {
@@ -60,7 +60,7 @@ fn bench_intern_fill(c: &mut Criterion) {
                     || BB::setup(),
                     |interner| {
                         for word in &words {
-                            black_box(interner.intern_and_hash(word));
+                            black_box(interner.intern(word));
                         }
                     },
                     BatchSize::SmallInput,
@@ -71,7 +71,7 @@ fn bench_intern_fill(c: &mut Criterion) {
     bench_for_backend::<BenchString>(&mut g);
 }
 
-fn bench_intern_already_filled(c: &mut Criterion) {
+fn bench_get_or_intern_already_filled(c: &mut Criterion) {
     let mut g = c.benchmark_group("intern/already-filled");
     g.throughput(Throughput::Elements(BENCH_LEN_STRINGS as u64));
     fn bench_for_backend<BB: BackendBenchmark>(g: &mut BenchmarkGroup<WallTime>) {
